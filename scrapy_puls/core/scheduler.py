d31ffa60dfe2ec0fcd7f32dcdf1dcc58bd2856d3
LHZ <lihuazongok@outlook.com>
2018-10-14 17:02:50 +0800

    实现调度器类 实现请求队列，缓存请求对象 请求去重待实现

---
+++ scrapy_puls/core/scheduler.py
@@ -0,0 +1,35 @@
+# 调度器
+
+# six: 第三方模块， 专门处理Python2 和 python3 的兼容
+from six.moves.queue import Queue
+
+
+class Scheduler(object):
+    """
+    功能
+    1 缓存请求
+    2 请求去重
+    """
+    def __init__(self):
+        # 创建队列， 用于缓存请求
+        self.queue = Queue
+
+    def add_request(self, request):
+        """把请求对象添加到队列"""
+        self.queue.put(request)
+
+    def get_request(self):
+        """将请求取出"""
+        self.queue.get()
+
+    def filter_requests(self, request):
+        """实现请求去重，如果该请求需要过滤就返回True，不需要过滤返回False"""
+        # TODU 待实现
+        pass
+
+
+
+
+
+
+
